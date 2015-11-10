27a8891d22888d0b80b7ba83b9f1bbf96cfe2fe2
Jack Tang <jackhftang@hotmail.com>
2015-11-10 23:03:37 +0800

    fix Besection.left

--- tests/bisection.test.js
+++ tests/bisection.test.js
@@ -21,16 +21,22 @@ module.exports = {
     bisection(large, 242).should.equal(4);
     bisection(large, 0).should.equal(0);
     bisection(large, 232424).should.equal(14);
+
   }
 , 'Bisection left': function(){
-    bisection.left(small, 4).should.equal(9);
-    bisection.left(small, 15).should.equal(0);
-    bisection.left(small, 4, 5).should.equal(9);
-    bisection.left(small, 12, 0, 5).should.equal(0);
-
-    bisection.left(large, 350).should.equal(0);
-    bisection.left(large, 242).should.equal(14);
-    bisection.left(large, 0).should.equal(14);
-    bisection.left(large, 232424).should.equal(0);
+    bisection.left(small, 4).should.equal(4);
+    bisection.left(small, 15).should.equal(9);
+    bisection.left(small, 4, 5).should.equal(5);
+    bisection.left(small, 12, 0, 5).should.equal(5);
+    bisection.left(large, 350).should.equal(8);
+    bisection.left(large, 242).should.equal(4);
+    bisection.left(large, 0).should.equal(0);
+    bisection.left(large, 232424).should.equal(14);
+  }
+, 'Bisection count': function(){
+    (bisection.right(small, 3) - bisection.left(small, 3)).should.equal(1);
+    (bisection.right(small, 4) - bisection.left(small, 4)).should.equal(0);
+    (bisection.right(large, 122) - bisection.left(large, 122)).should.equal(1);
+    (bisection.right(large, 4) - bisection.left(large, 4)).should.equal(0);
   }
 };
